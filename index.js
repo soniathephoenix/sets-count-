6bba25f1787e6081a4fba864040ebb39be3bc502
Sonia Pires <157916578+soniathephoenix@users.noreply.github.com>
2024-02-26 09:34:27 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 let countEl = document.getElementById("count-el")
 let count = 0
+let saveEl = document.getElementById("save-el")
+
+
 
 function increment() {
-    count = count + 1
-    countEl.innerText = count
+    count += 1
+    countEl.textContent = count
 }
 
+
+
 function save() {
-    console.log(count)
+    let countSave = count + " - "
+    saveEl.textContent += countSave
+    countEl.textContent = 0
+    count = 0
+
 }
+
+
